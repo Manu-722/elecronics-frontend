fc7f737778f7cb4c5cf5ee2705dc5d43544b4ec6
Manu-722 <Emmanuelmulongo46@gmail.com>
2026-02-20 17:42:03 +0300

    removed secrets

--- shoe-store/src/firebase.js
+++ shoe-store/src/firebase.js
@@ -1,25 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAFoje66tcTegGK1z41paScqhYaizRTbwI",
-  authDomain: "electrocity-1a568.firebaseapp.com",
-  projectId: "electrocity-1a568",
-  storageBucket: "electrocity-1a568.firebasestorage.app",
-  messagingSenderId: "622076114797",
-  appId: "1:622076114797:web:b787a0f63cf4fb3d5d4c64",
-  measurementId: "G-64LLNW37JC"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Export Firebase Auth instance
-export const auth = getAuth(app);
-
-export default app;
+export { app, analytics };
